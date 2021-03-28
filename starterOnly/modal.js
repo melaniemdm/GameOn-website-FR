c9e2ebda7e04fc68e6612b419b3157d6411236c3
melaniemdm <melanieborja.pro@gmail.com>
2021-03-28 16:49:58 +0200

    Feature(todo 1): close modal refacto(modal.js): commentaires

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,28 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
+// Element du DOM Elements
+//l'ensemble du formulaire (la page du formulaire)
+const modalbg = document.querySelector(".bground"); // permet de recuperer l'element de la class .bground
+//btn je m'inscrid
 const modalBtn = document.querySelectorAll(".modal-btn");
+//tous les elements du formulaire
 const formData = document.querySelectorAll(".formData");
+// création de la constante spanClose pour fermer la modale (Todo 1)
+const spanClose = document.querySelectorAll(".close"); 
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// launch modal event  (charger l'evement modal au niveau du btn)
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal)); // pour chaque btn contenu dans la class modal-btn on va lui ajouter un ecouteur d'evenement sur l'evenement click et lui demander de lancer la fonction launchModal lorsqu'un click est detecté
 
-// launch modal form
+// launch modal form (declaration fonction launchModal)
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = "block"; // prend la class bgroung et modifit le style et plus particulierement le display en none dans le fichier css
+  }
+ 
+// close modal event
+spanClose.forEach((btn) => btn.addEventListener("click", closeModal));
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
-
-
